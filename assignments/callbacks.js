b5d4f9d20914e25a774e011b32b680d8e5e8eb64
Tony <tonyrkovar@gmail.com>
2019-08-20 16:03:33 -0500

    working on ballback section

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,4 +1,7 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work. 
+//You have been provided an example of a problem and a solution to see how this works with 
+//our items array.  Study both the problem and the solution to figure out the rest of the 
+//problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
@@ -40,15 +43,25 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, function(length){
+  console.log(length);
+});
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-}
+  return cb(arr[arr.length-1])
+};
+
+last(items, function(arrLast){
+  console.log(arrLast);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  
 }
 
 function multiplyNums(x, y, cb) {
