08904b41624f13df70359caece07d41d7a4807ea
Tony <tonyrkovar@gmail.com>
2019-08-20 16:28:11 -0500

    finished the callback section stretch is unattempted

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -61,17 +61,35 @@ last(items, function(arrLast){
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-  
-}
+  return cb(x + y);
+};
+
+sumNums(20, 50, function(sumUp){
+  console.log(sumUp);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-}
+  return cb(x * y);
+};
+
+multiplyNums(200, 23165, function(multiplicitives){
+  console.log(multiplicitives);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-}
+  return cb(list.includes(item));
+};
+
+contains('clock', items, function(thisThing){
+  console.log(thisThing);
+});
+
+contains('Notebook', items, function(thisThing){
+  console.log(thisThing);
+});
 
 /* STRETCH PROBLEM */
 
